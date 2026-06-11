5843714c34541c17180ad04bb4154c99a4f4e46e
Kenya <kenyacobbs9001@gmail.com>
2026-06-11 13:07:45 -0400

    fixed code and added dropdown menu

--- src/components/CreateCourse.jsx
+++ src/components/CreateCourse.jsx
@@ -8,8 +8,10 @@ function Create (props) {
    const [end, setEnd] = useState("")
    
    //ok so I was super stuck and this i did get from claude
-   function handleSubmit() {
-    //This creates the object of the class schedule, spread operator online 
+   function handleSubmit(e) {
+    //This prevents me from having the form tag from working so my function can handle the submission of classes and time
+    e.preventDefault()
+        //This creates the object of the class schedule, spread operator online 
     const newClass = {
         name: name,
         day: day, 
@@ -22,23 +24,36 @@ function Create (props) {
     return (
         <div className="input-container">
             <div className="container-title">Add a course</div>
-            <p className ="input-title">Course name</p>
+            {/* because I added the form I had to insert the e.preventDefault() to my function */}
+            <form className="form">
+                <label placeholder="e.g.Intro to" className ="input-title">Course name</label>
+                <br></br>
             <input onChange={(e) => setName(e.target.value)} className="input">
             </input>
-            <p className="input-title">Day</p>
-            <input onChange={(e) => setDay(e.target.value)} className="input">
-            </input>
-            <p className="input-title">Start</p>
+            <label  className="input-title">Day</label>
+            <select onChange={(e) => setDay(e.target.value)} className="input">
+                <option></option>
+                <option> Monday </option>
+                <option> Tuesday </option>
+                <option> Wednesday </option>
+                <option> Thursday </option>
+                <option> Friday </option>
+                <option> Saturday</option>
+                <option> Sunday </option>
+            </select>
+            <label className="input-title">Start</label>
             <input onChange={(e) => setStart(e.target.value)}className="input">
             </input>
-            <p className="input-title">End</p>
+            <label className="input-title">End</label>
             <input onChange={(e) => setEnd(e.target.value)}className="input">
             </input>
             <div className="button-container">
-            <button onClick={() => handleSubmit()} className="add-class">
+            {/*Here i had to pass the event hanto access the prevent default method I referenced in my handle submit function */}
+            <button onClick={(e) => handleSubmit(e)} className="add-class">
                 Add Class
             </button>
             </div>
+            </form>
         </div>
     )
 }
